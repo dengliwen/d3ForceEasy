51e054c05c937f2174011bf50bc3f9fceaae65ea
DengLiWen <18801266814@163.com>
2021-01-18 17:43:26 +0800

    修改引用方式，增加test

--- README.md
+++ README.md
@@ -2,18 +2,39 @@
 < 一个封装D3js force简单使用的轮子>
 ### 使用方法
 ```
- import  {drawForce} from 'd3_force_easy'
+ import  d3ForceEasy from 'd3_force_easy'
  
  ...
  const option = {
-     dom:document.getElementById('forceView'),
+     dom:document.getElementById('app'),
      color:'#000',
      nodes:this.nodes,
      links:this.links
      ...
  }
  
- drawForce(option)
+ d3ForceEasy.drawForce(option)
+```
+
+### 测试数据
+```
+const nodes = [
+    {name:'2.2.2.2',id:12,type:'ip'},
+    {name:'3.4.25.22',id:13,type:'ip'},
+    {name:'13020002299',id:14,type:'phone'},
+    {name:'asdmklasdlnlwee.pdf',id:15,type:'file'},
+    {name:'sdf78f6s87d5fsud7fts87d6587r23grbusd7f78',id:16,type:'md5'},
+    {name:'15838837736',id:17,type:'phone'},
+]
+
+const links = [
+    {source:0,target:1},
+    {source:0,target:2},
+    {source:0,target:3},
+    {source:3,target:4},
+    {source:3,target:5},
+]
+
 ```
 
 升级改造中。。。
--- index.js
+++ index.js
@@ -1,88 +1,97 @@
-import * as d3 from 'd3'
-
-const defaultOption = {
-    dom:document.getElementsByTagName('body'),
-    color:'#000',
-    nodes:[],
-    links:[]
-}
-
-function drawForce(option = defaultOption){
-    const dom = option.dom;
-    const height = dom.offsetHeight;
-    const width = dom.offsetWidth;
-    const links = option.links;
-    const nodes = option.nodes;
-
-    const color = d => {
-        const scale = d3.schemeCategory10;
-        return scale[d.type];
-    }
-    const drag = simulation => {
-
-        function dragstarted(event) {
-            if (!event.active) simulation.alphaTarget(0.3).restart();
-            event.subject.fx = event.subject.x;
-            event.subject.fy = event.subject.y;
-        }
-
-        function dragged(event) {
-            event.subject.fx = event.x;
-            event.subject.fy = event.y;
-        }
-
-        function dragended(event) {
-            if (!event.active) simulation.alphaTarget(0);
-            event.subject.fx =  event.x;
-            event.subject.fy = event.y;
-        }
-
-        return d3.drag()
-            .on("start", dragstarted)
-            .on("drag", dragged)
-            .on("end", dragended);
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+        typeof define === 'function' && define.amd ? define(['exports'], factory) :
+            (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.d3ForceEasy = global.d3ForceEasy || {}));
+}(this, (function (exports) {
+    'use strict';
+    const defaultOption = {
+        dom: document.getElementsByTagName('body'),
+        color: '#000',
+        nodes: [],
+        links: []
     }
 
-    const simulation = d3.forceSimulation(nodes)
-        .force("link", d3.forceLink(links))
-        .force("charge", d3.forceManyBody())
-        .force("center", d3.forceCenter(width / 2, height / 2));
+    function drawForce(option = defaultOption) {
+        const dom = option.dom;
+        const height = dom.offsetHeight;
+        const width = dom.offsetWidth;
+        const links = option.links;
+        const nodes = option.nodes;
 
-    const svg = d3.select("#forceView").append('svg')
-        .attr("viewBox", [0, 0, width, height]);
+        const color = (d,i) => {
+            const scale = d3.schemeCategory10;
+            return scale[i];
+        }
+        const drag = simulation => {
 
-    const link = svg.append("g")
-        .attr("stroke", "#999")
-        .attr("stroke-opacity", 0.6)
-        .selectAll("line")
-        .data(links)
-        .join("line")
-        .attr("stroke-width",1);
+            function dragstarted(event) {
+                if (!event.active) simulation.alphaTarget(0.3).restart();
+                event.subject.fx = event.subject.x;
+                event.subject.fy = event.subject.y;
+            }
 
-    const node = svg.append("g")
-        .attr("stroke", "#fff")
-        .attr("stroke-width", 1.5)
-        .selectAll("circle")
-        .data(nodes)
-        .join("circle")
-        .attr("r", 5)
-        .attr("fill", color)
-        .call(drag(simulation));
+            function dragged(event) {
+                event.subject.fx = event.x;
+                event.subject.fy = event.y;
+            }
 
-    node.append("title")
-        .text(d => d.name);
+            function dragended(event) {
+                if (!event.active) simulation.alphaTarget(0);
+                event.subject.fx = event.x;
+                event.subject.fy = event.y;
+            }
 
-    simulation.on("tick", () => {
-        link
-            .attr("x1", d => d.source.x)
-            .attr("y1", d => d.source.y)
-            .attr("x2", d => d.target.x)
-            .attr("y2", d => d.target.y);
+            return d3.drag()
+                .on("start", dragstarted)
+                .on("drag", dragged)
+                .on("end", dragended);
+        }
 
-        node
-            .attr("cx", d => d.x)
-            .attr("cy", d => d.y);
-    });
-}
+        const simulation = d3.forceSimulation(nodes)
+            .force("link", d3.forceLink(links))
+            .force("charge", d3.forceManyBody())
+            .force("center", d3.forceCenter(width / 2, height / 2));
 
-export {drawForce}
+        const svg = d3.select(`#${option.dom.id}`).append('svg')
+            .attr("viewBox", [0, 0, width, height]);
+
+        const link = svg.append("g")
+            .attr("stroke", "#999")
+            .attr("stroke-opacity", 0.6)
+            .selectAll("line")
+            .data(links)
+            .join("line")
+            .attr("stroke-width", 1);
+
+        const node = svg.append("g")
+            .attr("stroke", "#fff")
+            .attr("stroke-width", 1.5)
+            .selectAll("circle")
+            .data(nodes)
+            .join("circle")
+            .attr("r", 5)
+            .attr("fill", color)
+            .call(drag(simulation));
+
+        node.append("title")
+            .text(d => d.name);
+
+        simulation.on("tick", () => {
+            link
+                .attr("x1", d => d.source.x)
+                .attr("y1", d => d.source.y)
+                .attr("x2", d => d.target.x)
+                .attr("y2", d => d.target.y);
+
+            node
+                .attr("cx", d => d.x)
+                .attr("cy", d => d.y);
+        });
+    }
+
+    exports.drawForce = drawForce;
+})))
+
+
+
+
